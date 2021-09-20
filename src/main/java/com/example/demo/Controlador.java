93443caa78065e259f3fb77709e17e1e3a6efbf8
arramos270 <alexrramos270@gmail.com>
2021-09-20 23:39:45 +0200

    Terminado ejercicio y corregido error con el nivel de lenguaje

--- src/main/java/com/example/demo/Controlador.java
+++ src/main/java/com/example/demo/Controlador.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
 public class Controlador {
+
 
     @GetMapping({"/", "index"})
     public String welcome(){
-
+        return "Bienvenido";
     }
+
+
+
 }
